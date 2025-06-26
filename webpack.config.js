8848313e4027c9b4c993d76743937bfec3514610
Eloi Bellet <eloi.bellet@ext.adeo.com>
2025-06-26 14:52:01 +0200

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ const webExtensionConfig = {
 	},
 	output: {
 		filename: '[name].js',
-		path: path.join(__dirname, './dist/web'),
+		path: path.join(__dirname, './dist-web/'),
 		libraryTarget: 'commonjs',
 		devtoolModuleFilenameTemplate: '../../[resource-path]'
 	},
